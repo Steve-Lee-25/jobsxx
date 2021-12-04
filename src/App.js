f053b5a3cd27e93d623aeae64951b58a4bc566d9
unknown <lamhuy251002@gmail.com>
2021-12-05 00:17:58 +0700

    Upload Source Code CV PROJECT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Register from "./Components/Register/Index";
+import Home from "./Components/Home/Index";
+import Signin from "./Components/Signin/Index";
+import Footer from "./Components/Footer/Index";
+import Reviews from "./Components/Reviews/Index";
+import ReviewsForm from "./Components/ReviewsForm/Index";
+import CVStudio from "./Components/CvStudio/Index";
+import Contact from "./Components/CvStudio/Contact/Index";
 
 function App() {
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cvtemplate" element={<CVStudio />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
